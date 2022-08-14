12634ab98945dd3b7bb8eefa6b8730ba8b8ae6f9
Alex Sander <46923948+maximatey@users.noreply.github.com>
2022-08-14 13:11:40 +0700

    Mengisi data dummy untuk kategori fabrics
    image belum diisi

--- src/src/components/dummy.js
+++ src/src/components/dummy.js
@@ -3,75 +3,86 @@ const Dummy = {
     data :
     [
         {
-            id: '1',
-            image : Image,
-            name : "Jeans Patched Fabric",
-            price : 50000,
-            description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            id:'1',
+            category : 'Fabric',
+            image : 
+            name : 'Jeans Patched Fabric',
+            price : 40000,
+            description : 'Surplus fabric collected from fashion production that is no longer needed by the factory that are patched into certain sizes.'
         },
         {
-            id: '2',
-            image : Image,
-            name : "Jeans Patched Fabric",
-            price : 50000,
-            description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            id:'2',
+            category : 'Fabrics',
+            image : 
+            name : 'Cotton Fabric'
+            price : 55000,
+            description : 'Residue cotton fabrics from the production of cotton-based wears.'
         },
         {
-            id: '3',
-            image : Image,
-            name : "Jeans Patched Fabric",
-            price : 50000,
-            description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            id:'3',
+            category : 'Fabrics',
+            image : 
+            name : 'Unprocessed Denim Fabric',
+            price : 75000,
+            description : 'Denim fabric that are leftover, not been processed after the first use.'
         },
         {
-            id: '4',
-            image : Image,
-            name : "Jeans Patched Fabric",
-            price : 50000,
-            description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            id:'4',
+            category : 'Fabrics',
+            image : 
+            name : 'Alternating Design Patched Fabric',
+            price : 60000,
+            description : 'Leftover fabrics from fashion productions that has been processed into an alternating design fabric.'
         },
         {
-            id: '5',
-            image : Image,
-            name : "Jeans Patched Fabric",
-            price : 50000,
-            description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            id:'5',
+            category : 'Fabrics',
+            image : 
+            name : 'Cotton Doubl Layer Knitted Fabric',
+            price : 60000,
+            description : 'Leftover fabric knitted to sized fabric in a double-layered style.'
         },
         {
             id: '6',
-            image : Image,
-            name : "Jeans Patched Fabric",
-            price : 50000,
-            description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            category : 'Fabrics',
+            image : 
+            name : 'Fixed Sized Denim Fabric',
+            price : 12000,
+            description : 'Denim fabric residue left from wear production, in fixed sizes.'
         },
         {
-            id: '7',
-            image : Image,
-            name : "Jeans Patched Fabric",
-            price : 50000,
-            description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            id:'7',
+            category : 'Fabrics',
+            image : 
+            name : 'Polyester Fabric',
+            price : 90000,
+            description :'Fabric leftover from the production of a European bag manufacturer.'
         },
         {
             id: '8',
-            image : Image,
-            name : "Jeans Patched Fabric",
-            price : 50000,
-            description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            category : 'Fabrics',
+            image : 
+            name : 'Patched Fabric Scraps',
+            price : 30000,
+            description : 'Fabric scraps patched into certain sizes made for upcycling.'
         },
         {
-            id: '9',
-            image : Image,
-            name : "Jeans Patched Fabric",
-            price : 50000,
-            description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            id:'9',
+            category : 'Fabrics',
+            image : 
+            name : 'Nylon Fabric',
+            price : 110000,
+            description : 'Nylon fabric residue left from the production from a wear manufacturer.'
         },
         {
-            id: '10',
-            image : Image,
-            name : "Jeans Patched Fabric",
-            price : 50000,
-            description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            id:'10',
+            category : 'Fabrics',
+            image : 
+            name : 'Linen Cotton Cuts',
+            price : 45000,
+            description : 'Leftover fabric linen cotton cuts.'
         },
+
         {
             id: '11',
             image : Image,
