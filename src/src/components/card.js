2dc7e1ca446b43946eb7cb8ffea9eb69e0da2c5d
unknown <16521344@mahasiswa.itb.ac.id>
2022-08-15 12:38:30 +0700

    upload card

--- src/src/components/card.js
+++ src/src/components/card.js
@@ -1,6 +1,38 @@
-const Card = () => {
+import { db } from "../firebase";
+import { doc, updateDoc } from "firebase/firestore"; 
+
+const Card = (props) => {   
+    const updateCart = async () => {
+        window.alert("Produk berhasil ditambahkan ke cart.")
+        let cart = {
+            id:props.id,
+            image: props.image,
+            name:props.name,
+            price:props.price,
+            quantity: 1
+        }
+        let newCart = props.cart
+        newCart.push(cart)
+        console.log(newCart)
+        const washingtonRef = doc(db, "users", props.doc);
+
+        await updateDoc(washingtonRef, {
+            cart : newCart
+        });
+
+    }
     return (
-        <div></div>
+            <div className="card">
+            <a href={`/products/${props.id}`}><img src={props.image} alt="fabric" /></a>
+            <p>{props.name}</p>
+            <div className="card-bottom">
+                <div className="card-price">
+                    <span style={{fontSize:"12px"}}>Price</span>
+                    <span>Rp{props.price}/kg</span>
+                </div>
+                <button onClick={updateCart}>Add to cart</button>
+            </div>
+            </div>
     )
 }
 
