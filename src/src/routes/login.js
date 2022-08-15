336e325475ef1eb1920838942a0c62b332093698
Fredrick Taslim <16521399@mahasiswa.itb.ac.id>
2022-08-16 02:38:37 +0700

    update login and register

--- src/src/routes/login.js
+++ src/src/routes/login.js
@@ -1,6 +1,48 @@
 const Login = () => {
     return (
-        <div></div>
+        <div class="login_page">
+            <div class="login_container">
+                <div class="brand_logo">
+                    <img src="../images/logo.png">
+                </div>
+
+    {/* <!-- Main Form --> */}
+                <div class="main">
+                    <div class="form_input">
+                        <h1>Welcome!</h1><br>
+                        <form>
+                            <div class="user_input">
+                                <label for="email">Email</label><br>
+                                <input 
+                                    type="text" 
+                                    id="email" 
+                                    placeholder="pasukanhmif@gmail.com"><br>
+                                <label for="psw">Password</label><br>
+                                <input type="password" id="psw"><br><br>
+                            </div>
+                            <div class="login_btn">
+                                <a href="register.html">
+                                    <button type="submit">Login</button>
+                                </a>
+                            
+                            </div>
+                            <p>Not registered? <a href="register.html"><strong>Create an account</strong></a></p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+    {/* <!-- Footer --> */}
+            <div class="footer">
+                <br><br>
+                <p1>About Us Privacy Policy</p1>
+                <br>
+                <p2>Contact Us</p2>
+                <br><br>
+                <p3>By Spartans made with ❤️</p3>
+                <br><br>
+            </div>
+        </div>
     )
 }
 
--- src/src/routes/register.js
+++ src/src/routes/register.js
@@ -1,6 +1,54 @@
 const Register = () => {
     return (
-        <div></div>
+        <div class="regis_page">
+            <div class="regis_container">
+                <div class="brand_logo">
+                    <img src="../images/logo.png">
+                </div>
+
+    {/* <!-- Main Form --> */}
+                <div class="main">
+                    <div class="form_input">
+                        <h1>Register</h1><br>
+                        <form>
+                            <div class="user_input">
+                                <label for="email">Email</label><br>
+                                <input 
+                                    type="text" 
+                                    id="email" 
+                                    placeholder="pasukanhmif@gmail.com"><br>
+                                <label for="uname">Username</label><br>
+                                <input 
+                                    type="text"
+                                    id="uname"
+                                    placeholder="spartans_21"><br>
+                                <label for="psw">Password</label><br>
+                                <input type="password" id="psw">
+                                <p>Min. 5 characters, must be a combination of letter and number</p><br>
+                                <label for="psw2">Confirm Password</label><br>
+                                <input type="password" id="psw2"><br>
+                            </div>
+                            <div class="regis_btn">
+                                <a href="login.html">
+                                    <button type="submit">Register</button>
+                                </a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+    {/* <!-- Footer --> */}
+            <div class="footer">
+                <br><br>
+                <p1>About Us Privacy Policy</p1>
+                <br>
+                <p2>Contact Us</p2>
+                <br><br>
+                <p3>By Spartans made with ❤️</p3>
+                <br><br>
+            </div>
+        </div>
     )
 }
 
