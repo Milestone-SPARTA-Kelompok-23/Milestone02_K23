fd3a28883aac99edd39e17ef30b67cae2a4ec3f1
unknown <16521486@mahasiswa.itb.ac.id>
2022-08-15 15:39:03 +0700

    our product

--- src/src/routes/products.js
+++ src/src/routes/products.js
@@ -1,7 +1,69 @@
+import Pagination from '../components/pagination';
+import Card from "../components/card";
+import NavBar from '../components/navbar';
+import Footer from '../components/footer';
+import React, { useState, useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Data from "../components/dummy";
+import { auth, db } from "../firebase";
+import { query, collection, getDocs, where } from "firebase/firestore";
+
 const Products = () => {
-    return (
-        <div></div>
-    )
+    const [user, loading] = useAuthState(auth);
+    const [cart, setCart] = useState([]);
+    const [category, setCategory] = useState("Fabrics");
+    const [id, setID] = useState("");
+    useEffect(() => {
+        if(user) {
+                const fetchData = async () => {
+                    const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+                    const doc = await getDocs(q);
+                    const data = doc.docs[0].data();
+                    setID(doc.docs[0].id);
+                    if(data.cart) setCart(data.cart);
+                }
+                if (loading) return;
+                fetchData();
+        }
+    }, [user, loading]);
+    const fetchCategory = (e) => {
+        const category = e
+        setCategory(category)
+    }
+        return (
+            <div className='products'>
+                <NavBar />
+                <h1>Our {category}</h1>
+                <div className='main-products'>
+                    <div className='categories'>
+                        <h2>Categories</h2>
+                        <table>
+                            <tr onClick={() => fetchCategory('Fabrics')}>
+                                <td>Fabrics</td>
+                            </tr>
+                            <tr onClick={() => fetchCategory('Zero-Waste Fashion')}>
+                                <td>Zero-Waste Fashion</td>
+                            </tr>
+                            <tr onClick={() => fetchCategory('Thrift')}>
+                                <td>Thrift</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <Pagination
+                        data={Data}
+                        RenderComponent={Card}
+                        title="Posts"
+                        pageLimit={2}
+                        dataLimit={6}
+                        cart={cart}
+                        updateCart={setCart} 
+                        doc={id}
+                        category={category}
+                    />
+                </div>
+                <Footer />
+            </div>
+        )
 }
 
 export default Products;
