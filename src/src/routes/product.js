1b1207f8d63018116eda8c721b75407ae20563ce
SHaNM3 <farhan.alganiputra@gmail.com>
2022-08-15 12:07:03 +0700

    upload profile and product details

--- src/src/routes/product.js
+++ src/src/routes/product.js
@@ -1,6 +1,56 @@
-const Product = () => {
+import { useParams } from "react-router-dom";
+import NavBar from "../components/navbar";
+import Footer from "../components/ footer";
+import "../styles/product.css";
+import { db } from "../firebase";
+import { doc, updateDoc} from "firebase/firestore";
+
+const Product = (props) => {
+    const {id} = useParams();
+    const data = props.data.data.filter(item => item.id === id)[0];
+    console.log(data)
+    const updateCart = async () => {
+        window.alert("Produk berhasil ditambahkan ke cart.")
+        let cart = {
+            id:data.id,
+            image:data.image,
+            name:data.name,
+            price:data.price
+        }
+        let newCart = props.cart
+        newCart.push(cart)
+        const washingtonRef = doc(db, "users", props.doc);
+
+        await updateDoc(washingtonRef, {
+            cart : newCart
+        });
+
+    }
     return (
-        <div></div>
+        <div>
+            <NavBar />
+            <div className="product">
+                <div className="back">
+                    <a href="/products">&#8592;</a>
+                </div>
+                <div className="main-product">
+                    <div>
+                        <div className="product-image">
+                            <img src={data.image} alt="product" />
+                        </div>
+                    </div>
+                    <div className="product-details">
+                        <h1>{data.name}</h1>
+                        <hr></hr>
+                        <p>Description :</p>
+                        <p>{data.description}</p>
+                        <p className="price-product">Price:<br></br><span>Rp.{data.price}/kg</span></p>
+                        <button onClick={updateCart}>Add to cart</button>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
     )
 }
 
--- src/src/routes/profile.js
+++ src/src/routes/profile.js
@@ -1,7 +1,79 @@
-const Profile = () => {
-    return (
-        <div></div>
-    )
-}
+import React, { userEffect, userSatte } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import "../styles/dashboard.css";
+import { auth, db, logout } from "../firebase";
+import NavBar from "../components/navbar";
+import Footer from "../components/footer";
+import Profile from "../images/profile-img.png"
+import { query, collection, getDocs, where } from "firebas/firestore";
+function Dashboard() {
+    const [user, loading] = useAuthState(auth);
+    const [name, setname] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("")
+    const [address, setAddress] = useState("");
+    const navigate = useNavigate();
 
-export default Profile;
+    useEffect(() => {
+        const fetchUserName = async () => {
+        try {
+            const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+            const doc = await getDocs(q);
+            const data = doc.docs[0].data();
+            setname(data.name);
+            setUsername(data.username);
+            setEmail(data.email);
+            setPhoneNumber(data.phoneNumber);
+            setAddress(data.address);
+        }   catch (err) {
+            console.error(err);
+            alert("An error occured while fetching user data");
+        }
+    };
+        if (loading) return;
+        if (!user) return navigate("/");
+        fetchUserName();
+    }, [user, loading, navigate])
+    return (
+        <>
+        <NavBar />
+            <div className="dashboard">
+                <div className="dashboard__container">
+                    <div className="profile-header">
+                        <img src={Profile} alt="profile" />
+                        <h1>Profile</h1>
+                    </div>
+                    <table>
+                        <tr>
+                            <td className="table-header">Username</td>
+                            <td className="data-profile">{username}</td>
+                        </tr>
+                        <tr>
+                            <td className="table-header">Name</td>
+                            <td className="data-profile">{name}</td>
+                        </tr>
+                        <tr>
+                            <td className="table-header">Email</td>
+                            <td className="data-profile">{email}</td>
+                        </tr>
+                        <tr>
+                            <td className="table-header">Phone Number</td>
+                            <td className="data-profile">{phoneNumber}</td>
+                        </tr>
+                        <tr>
+                            <td className="table-header">Address</td>
+                            <td className="data-profile">{address}</td>
+                        </tr>
+                    </table>
+                    <button className="dashboard__btn" onClick={logout}>
+                        Logout
+                    </button>
+                </div>
+            </div>
+        <Footer />
+        </>
+    );
+}
+export default Dashboard;
